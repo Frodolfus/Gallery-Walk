0ea56299102015744191986520e49aad31194886
Frodolfus <132409592+Frodolfus@users.noreply.github.com>
2023-05-03 01:10:42 +0200

    Gallery Walk

---
+++ gallery-walk-zehn-gebote---sinn--design/app-files/data.js
@@ -0,0 +1,578 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-start",
+      "name": "Start",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.5387354679734913
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.643536546388976,
+          "pitch": 0.036130498437042746,
+          "rotation": 0,
+          "target": "1-station-1"
+        },
+        {
+          "yaw": -0.14359483166396458,
+          "pitch": 0.04766417859703509,
+          "rotation": 0,
+          "target": "5-station-5"
+        },
+        {
+          "yaw": -0.6478212715734735,
+          "pitch": 0.06467126912923504,
+          "rotation": 0,
+          "target": "4-station-4"
+        },
+        {
+          "yaw": -1.2911240480902606,
+          "pitch": 0.08508422507581592,
+          "rotation": 0,
+          "target": "3-station-3"
+        },
+        {
+          "yaw": 1.2692656083207243,
+          "pitch": 0.05601126008702906,
+          "rotation": 0,
+          "target": "2-station-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-station-1",
+      "name": "Station 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": 3.1231668902256224,
+        "pitch": 0.1253793292567167,
+        "fov": 1.5387354679734913
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5093934411596095,
+          "pitch": 0.0787982932931186,
+          "rotation": 0,
+          "target": "2-station-2"
+        },
+        {
+          "yaw": -0.5786904721022061,
+          "pitch": 0.07999989942516983,
+          "rotation": 0,
+          "target": "0-start"
+        },
+        {
+          "yaw": -0.10942863824382698,
+          "pitch": 0.3617890269989985,
+          "rotation": 0,
+          "target": "7-ungestrter-blick"
+        },
+        {
+          "yaw": 0.04206286185925023,
+          "pitch": 0.031125321967206077,
+          "rotation": 0,
+          "target": "3-station-3"
+        },
+        {
+          "yaw": 0.5048888267157245,
+          "pitch": 0.01376453596597571,
+          "rotation": 0,
+          "target": "4-station-4"
+        },
+        {
+          "yaw": 0.8620645157261659,
+          "pitch": -0.00042497759304716,
+          "rotation": 0,
+          "target": "5-station-5"
+        },
+        {
+          "yaw": -0.2527915534691676,
+          "pitch": 0.04325368628299131,
+          "rotation": 0,
+          "target": "6-bcherstand"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.8988529938578456,
+          "pitch": -0.07992999752398688,
+          "title": "Wenn du mich liebst, wirst du nicht begehren...",
+          "text": "\"Die Augen gieren, die Hände greifen nach immer mehr. Die Hände stehen für die Handlung, das Nehmen, das Haben. Die Augen für das Begehren. Aber nehme ich wirklich wahr, was ich begehre und besitzen will?\" - Katharina Müller"
+        },
+        {
+          "yaw": -2.5509033320809387,
+          "pitch": -0.05439917268018135,
+          "title": "Wenn du mich liebst, wirst du des Sabbats gedenken.",
+          "text": "\"Diese 'Strichmännchen', weibliche und männliche Figuren im Hamsterrad des Alltags, sind eifrig und schwer beschäftigt: alles in einer engen Taktung. Als sei ihr stetes Tun fast wie eine Flucht. Mir war klar: Es geht im dritten Gebot um Entschleunigung. Darum, die eigene Identität zu finden und zu feiern. Dabei rückt die Gemeinschaft in den Mittelpunkt - die Beziehung zu sich selbst und zu den anderen. Du powerst vom ersten bis zum sechsten Tag - nonstop. Der siebte Tag ist etwas Heiliges, das bleibt, das musst du dir nehmen. Man müllt sich sonst so zu. Es braucht die innere Stimme, die ruft: 'Auszeit!' Und da steht dann der Mensch im Mittelpunkt.&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "2-station-2",
+      "name": "Station 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": 3.071050313619317,
+        "pitch": 0.21347776269393215,
+        "fov": 1.5387354679734913
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.24606494310638283,
+          "pitch": 0.4446905570980668,
+          "rotation": 0,
+          "target": "7-ungestrter-blick"
+        },
+        {
+          "yaw": -1.305268895075585,
+          "pitch": 0.11904973829942733,
+          "rotation": 0,
+          "target": "0-start"
+        },
+        {
+          "yaw": -0.8094511950268917,
+          "pitch": 0.009845054014091303,
+          "rotation": 0,
+          "target": "3-station-3"
+        },
+        {
+          "yaw": -0.2585828749985968,
+          "pitch": 0.028120301600553788,
+          "rotation": 0,
+          "target": "4-station-4"
+        },
+        {
+          "yaw": 0.2002456760353688,
+          "pitch": 0.009010226866740467,
+          "rotation": 0,
+          "target": "5-station-5"
+        },
+        {
+          "yaw": -1.1785095410545647,
+          "pitch": 0.03882994429197062,
+          "rotation": 0,
+          "target": "6-bcherstand"
+        },
+        {
+          "yaw": 1.2731911260736162,
+          "pitch": -0.002094400424880405,
+          "rotation": 0,
+          "target": "1-station-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.835955021887104,
+          "pitch": 0.07050944423700756,
+          "title": "Wenn du mich liebst, wirst du nicht begehren...",
+          "text": "\"Die Spirale, ein Sog. Im Zentrum der Schnuller, dann ringsherum sehr viele Gegenstände aus Konsum und Werbung. Die Waren drehen sich von Anbeginn des Lebens um die Person, die zum Konsumenten degradiert wird. Ein Sog, der Menschen von sich selbst wegzieht. [...] Einschränkungen zu kennen, das geht uns mitunter verloren - mir auch. Natürlich sind Armut und Mangel nicht an sich etwas Gutes. Aber Mangel kann uns lehren, etwas wertzuschätzen. Doch hier: Die Spirale des Begehrens dreht, ein Sog, der mich nicht loslässt.\" - Katharina Müller"
+        }
+      ]
+    },
+    {
+      "id": "3-station-3",
+      "name": "Station 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -3.041706117937201,
+        "pitch": 0.12554873269455413,
+        "fov": 1.5387354679734913
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.099558609720521,
+          "pitch": 0.04789427209334107,
+          "rotation": 0,
+          "target": "6-bcherstand"
+        },
+        {
+          "yaw": 0.768429940790325,
+          "pitch": 0.12451768925583373,
+          "rotation": 0,
+          "target": "0-start"
+        },
+        {
+          "yaw": 0.09872743658745975,
+          "pitch": 0.009761897668155939,
+          "rotation": 0,
+          "target": "2-station-2"
+        },
+        {
+          "yaw": -0.5121276360763147,
+          "pitch": -0.010264600110119204,
+          "rotation": 0,
+          "target": "1-station-1"
+        },
+        {
+          "yaw": -1.2259182952657852,
+          "pitch": -0.0021992794246035174,
+          "rotation": 0,
+          "target": "5-station-5"
+        },
+        {
+          "yaw": -1.846919843687953,
+          "pitch": -0.031555411426111846,
+          "rotation": 0,
+          "target": "4-station-4"
+        },
+        {
+          "yaw": -0.49266446443156653,
+          "pitch": 0.4229531820915433,
+          "rotation": 0,
+          "target": "7-ungestrter-blick"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.026921205995319,
+          "pitch": -0.008970763287379668,
+          "title": "Wenn du mich liebst, wirst du nicht töten.&nbsp;",
+          "text": "\"Die Extreme von Weiß und Bunt, der Kontrast von etwas Lebendigem und etwas Totem. Da ist auf der einen Seite etwas leer und starr, etwas ausgelöscht, wie ausgesaugt. Da ist nichts mehr, was fühlt, worauf man zurückgreifen kann. Und auf der anderen Seite sind die Gefühle, das Leben, so wie es eben ist: das Bunte, die höhen und Tiefen. [...] Von allen Geboten war das für mich das heftigste. Wenn jemand das übertritt, ist das am schlimmsten. Zugleich spricht das Bild anscheinend viele Betrachter*innen an. Viele melden mir zurück, es sei das schönste Bild aus der Reihe. Eigenartig. - Katharina Müller"
+        },
+        {
+          "yaw": -2.3088054215651734,
+          "pitch": -0.015604160003977796,
+          "title": "Wenn du mich liebst, wirst du nicht stehlen.&nbsp;",
+          "text": "\"Und zugleich habe ich das Gebot in unsere Welt geholt: Was wird uns gestohlen? Zeit! Der Blick der Betrachter*innen fällt auf den Frühstückstisch, als ob er gleich zugreifen könnte. Die mit Acryltechnik gemalte Szene lenkt die Perspektive auf die Tischplatte und holt die Betrachter*innen ins Bild. Du wirst Teil des Tisches. Du bist betroffen! Die Anfrage nach Zeit ist eine Anfrage an dich. auch zugegen: Die reale Außenwelt. Im Löffel spiegelt sie sich wider. Die Außenwelt holt dich ein und will deine Zeit.\" - Katharina Müller"
+        }
+      ]
+    },
+    {
+      "id": "4-station-4",
+      "name": "Station 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -3.105904678285132,
+        "pitch": 0.07570761244943469,
+        "fov": 1.5387354679734913
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6271421223848037,
+          "pitch": 0.01864276256173092,
+          "rotation": 0,
+          "target": "3-station-3"
+        },
+        {
+          "yaw": 1.3139867269189338,
+          "pitch": 0.02444986821766193,
+          "rotation": 0,
+          "target": "6-bcherstand"
+        },
+        {
+          "yaw": 0.5392126269343986,
+          "pitch": 0.041400067151130315,
+          "rotation": 0,
+          "target": "0-start"
+        },
+        {
+          "yaw": -0.28734035444295003,
+          "pitch": 0.5155295149372225,
+          "rotation": 0,
+          "target": "7-ungestrter-blick"
+        },
+        {
+          "yaw": -0.2681551863079772,
+          "pitch": 0.022175387449552275,
+          "rotation": 0,
+          "target": "2-station-2"
+        },
+        {
+          "yaw": -0.9394405554150627,
+          "pitch": 0.000830426389327954,
+          "rotation": 0,
+          "target": "1-station-1"
+        },
+        {
+          "yaw": -1.7088855187796312,
+          "pitch": -0.023732045732726803,
+          "rotation": 0,
+          "target": "5-station-5"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.0007751963043283,
+          "pitch": -0.13291637062906148,
+          "title": "Wenn du mich liebst, wirst du nicht Ehe brechen.",
+          "text": "\"Ich habe mit Acryl auf holz gemalt. Stein für Stein wird mit Freude und mit Mühe an einem Haus gebaut - wie in einer Ehe oder in jeder anderen Beziehung. Dann wurde mit einer Axt ins Bild geschlagen. Die Zerstörung sollte fassbar werden, sie lediglich zu malen, hätte nicht gereicht. Ein echter Bruch. Man sieht die Holzsplitter, kann sie berühren. Holz kann man mit Leim sanieren, aber man wird den Bruch noch sehen. Der Riss wird immer da sein. Jedoch will ich nicht werten. Manchmal kann man in dem 'Haus', in das man gemeinsam Zeit, Geld, Gefühle investiert hat, einfach nicht mehr leben. Vielleicht muss man dann einfach ausbrechen. Ein Befreiungsschlag, irgendwie.\" - Katharina Müller"
+        },
+        {
+          "yaw": -2.441803345572234,
+          "pitch": -0.09509887028846897,
+          "title": "Wenn du mich liebst, wirst du meinen Namen JHWH, den Namen deines Gottes, nicht zum Trug aussprechen.",
+          "text": "\"Immerzu musste ich beim Zeichnen an meine eigene Kindheit denken, meine religiöse Erziehung ploppte auf: dieser Gott, der alles sieht: Gott, dessen Name ich nicht missbrauchen soll. Bei der Beichte als Kind habe ich mich dann gefragt: 'Was soll ich sagen? Ob ich irgendwie seinen Namen missbraucht habe, womöglich ohne es zu wissen? Aber warum soll ich etwas sagen, wenn er doch eh alles sieht?' Dieser Gott hatte sich unlängst in mein tiefes Inneres geschlichen und sich in mein Gewissen eingenistet. 'Wenn ich mit mir selbst im Reinen bin, bedroht mich das ja nicht.', dachte ich. Und da war es wieder: das stete Gefühl seines Blickes...\" - Katharina Müller"
+        }
+      ]
+    },
+    {
+      "id": "5-station-5",
+      "name": "Station 5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": 3.1142744566020752,
+        "pitch": 0.13749198582770106,
+        "fov": 1.5387354679734913
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.013857800387944152,
+          "pitch": 0.12300878756736289,
+          "rotation": 0,
+          "target": "0-start"
+        },
+        {
+          "yaw": 1.8115929435733626,
+          "pitch": 0.016635001500542757,
+          "rotation": 0,
+          "target": "4-station-4"
+        },
+        {
+          "yaw": 0.909566224713295,
+          "pitch": 0.06763727667379804,
+          "rotation": 0,
+          "target": "3-station-3"
+        },
+        {
+          "yaw": -1.0059593560696598,
+          "pitch": 0.02314714089330394,
+          "rotation": 0,
+          "target": "2-station-2"
+        },
+        {
+          "yaw": -1.7828680458699502,
+          "pitch": 0.012915517484254124,
+          "rotation": 0,
+          "target": "1-station-1"
+        },
+        {
+          "yaw": 0.5464621491833128,
+          "pitch": 0.08536788311077892,
+          "rotation": 0,
+          "target": "6-bcherstand"
+        },
+        {
+          "yaw": -0.5629990280923796,
+          "pitch": 0.6622092994496036,
+          "rotation": 0,
+          "target": "7-ungestrter-blick"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.5140362600832233,
+          "pitch": 0.030472368732723965,
+          "title": "Wenn du mich liebst, wirst du neben mir keine anderen Götter haben.",
+          "text": "\"Das erste Gebot habe ich dezidiert abstrakt gestaltet: Was wissen wir schon über Gott? [...] Das erste Gebot ist für mich zugleich wie ein roter Faden: der weg, den Gott mir weist. Gott ist mittendrin - rings herum das Leben in all seinen Schattierungen\" - Katharina Müller"
+        },
+        {
+          "yaw": 2.992778552641779,
+          "pitch": 0.017528378294095148,
+          "title": "Wenn du mich liebst, wirst du deinen Vater und deine Mutter ehren.",
+          "text": "\"'Ehre deine Eltern!' Das Gebot ärgert mich! Es drängt mich zu fragen: Was ist mit den Kindern? Wo bleibt dieser Teil des Generationenvertrags? Wenn ich mein Kind jahrelang nicht beachtet habe, kann ich nicht erwarten, dass mein Kind mich ehrt. Es geht um ein Gleichgewicht von einer Generation zur anderen, ein Gleichgewicht zwischen Eltern und Kindern. [...] Wenn das Gleichgewicht zwischen den Generationen nicht funktioniert, gerät der Kreislauf zwischen den Generationen aus der Balance.\" - Katharina Müller"
+        }
+      ]
+    },
+    {
+      "id": "6-bcherstand",
+      "name": "Bücherstand",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -3.0360854800141244,
+        "pitch": 0.2611895795281072,
+        "fov": 1.5387354679734913
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.5800104912176316,
+          "pitch": 0.25057714243694207,
+          "title": "Zehn Gebote. Sinn &amp; Design",
+          "text": "Haben Moses und das Volk Israel von Gott die Zehn Gebote erhalten? Oder ist das alles nur ein frommes Märchen? Wann wurden die Gebote für wen niedergeschrieben? Zu welchem Zweck? Und vor allem: Wie sind die Gebote heute zu verstehen? Sind sie überhaupt noch von Bedeutung?<div>Im Zusammenspiel von Kunst, Bibelwissenschaft und Aktualisierung bietet das Autor*innenteam Zugänge zu biblischen Traditionen und lässt sie für Leser*innen des 21. Jahrhunderts lebendig werden. Ein Buch zum Stöbern, zum Verweilen, zum Entdecken: wissenschaftlich fundiert, aktuell und lebensnah - mit viel Design!<br></div>"
+        }
+      ]
+    },
+    {
+      "id": "7-ungestrter-blick",
+      "name": "Ungestörter Blick",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": 1.8352832998035158,
+        "pitch": -0.04049971266417707,
+        "fov": 1.5387354679734913
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.12011583761480615,
+          "pitch": 0.14561362618282558,
+          "rotation": 0,
+          "target": "0-start"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Gallery Walk: Zehn Gebote - Sinn & Design",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
